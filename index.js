c83b798c7a420544ecae1860917b0f6bda93d08e
r1tikpatil <ritikpatil566@gmail.com>
2023-12-02 18:53:23 +0530

    app server

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT;
+
+const db = require("./config/mongoose");
+
+app.use(express.json());
+app.use("*", cors());
+
+app.use("/api/user", require("./routes/user"));
+app.use("/api/loan", require("./routes/loan"));
+
+app.listen(PORT, () => {
+  console.log("Server is listening on port " + PORT);
+});
